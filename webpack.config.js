d0d72dae1b3a0753f6c754d8a539db686c30f1a5
leon <leon.vinogradov@gmail.com>
2020-06-10 02:20:24 +0300

    chore(build): enable library build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path')
 
-module.exports = [
-  {
-    name: 'library',
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-      filename: 'geohex.min.js',
-      path: path.resolve(__dirname, 'dist')
-    }
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'geohex.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'geohex',
+    libraryTarget: 'umd',
+    globalObject: 'this'
   }
-]
+}
